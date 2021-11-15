464763a53bc9d582e001b74326c49f1a4b51d397
Alejandro F <afuste7@gmail.com>
2021-11-15 16:53:36 -0500

    added new python file with basic practice code

---
+++ control_flow.py
@@ -0,0 +1,55 @@
+# name = 'Alejandro Fuste'
+# my_number = 5
+# phrase = 'Hello World!'
+#
+# print(phrase)
+
+# x = 6
+# y = 6
+#
+# if x < y:
+#     print("Y is larger")
+# elif x == y:
+#     print('They are the same value')
+# else:
+#     print('X is larger')
+
+my_name = "Eric"
+sibling_name = "Sam"
+
+# if my_name is sibling_name:
+#     print("we have the same name")
+# else:
+#     print('we do not have the same name')
+
+
+# if my_name is not sibling_name:
+#     print("we have the same name")
+# else:
+#     print('we do not have the same name')
+
+# x = 5
+# y = 7
+# z = 43
+#
+# if x < 7 and x > z:
+#     print('neat')
+# elif x < y and x < z:
+#     print('that is a lot of comparisons')
+
+# greeting = "Hello"
+# name = 'Eric'
+#
+# print(greeting + name)
+# print(greeting + ' ' + name)
+# print(f'Hello {name}!')
+
+full_name = 'Alejandro Fuste'
+print(full_name[0:9])
+print(full_name[10:])
+
+print(full_name[0::2])
+print(full_name[::-1])
+
+
+
