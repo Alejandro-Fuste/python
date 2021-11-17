16c32ab422a5d9549fe488b11dcde9cf088d6467
Alejandro F <afuste7@gmail.com>
2021-11-17 14:46:50 -0500

    added notes for exceptions

---
+++ DataStructures/exceptions.py
@@ -0,0 +1,9 @@
+"""
+Exceptions:
+    are errors in other languages except for syntax errors
+    don't call them errors unless it's an syntax error
+
+    Try and Except Block ~ 2:22
+    raise ~ 2:40
+
+"""
