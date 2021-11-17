cd9763788d3738b163439ade05dadf0ff795cda4
Alejandro F <afuste7@gmail.com>
2021-11-17 11:50:43 -0500

    added notes for lists and new sets file

---
+++ DataStructures/dataStructures.py
@@ -0,0 +1,31 @@
+# Data Structures
+
+# Common data structures for arrays: lists, sets, dictionaries, tuples
+
+# Lists
+
+    # adding to list
+        # insert method
+        # append method ~ 10:40
+        # extend method ~ 10:42
+
+    # removing from list
+        # remove method
+        # pop method - same as remove, but returns the popped value
+        # clear method
+        # del keyword
+
+    # length of list
+        # count method ~ 10:53
+        # len method
+        # index method ~ 10:57
+        # sort ~ 11:00
+        # sort by class ~ 11:27
+        # reverse ~ 11:40
+
+    # copy list ~ 11:48
+
+
+
+
+
