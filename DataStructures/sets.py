755321a5e5f72e6d003bccf8537067f91c78bdb6
Alejandro F <afuste7@gmail.com>
2021-11-17 12:44:38 -0500

    added notes for sets and tuples

---
+++ DataStructures/sets.py
@@ -0,0 +1,13 @@
+# Sets ~ 11:51
+
+    # mutable
+    # no duplicates and indexing
+
+# Methods
+
+    # add ~ 11:54
+    # pop -> removes a random item
+    # remove & discard
+    # update ~ 12:25
+
+
---
+++ DataStructures/tuples.py
@@ -0,0 +1,14 @@
+""" Tuples
+
+    - immutable
+    - indexable
+    - duplicates allowed
+"""
+"""
+    Methods
+
+        count ~ 12:37
+        index ~ 12:39
+"""
+
+
