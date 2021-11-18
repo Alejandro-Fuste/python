f8020a1a98fa1444577dedaa03ccc0bd59268e0e
Alejandro F <afuste7@gmail.com>
2021-11-18 18:02:32 -0500

    added code for flask

---
+++ Flask/flask.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+"""
+Servers_with_flask:
+    what we will use to create a server
+
+    How:
+        - install flask in pip & then import it
+
+"""
+
+
+
+app = Flask(__name__)
+
+@app.get("/")
+def landing_page():
+    return "This is the landing page"
+
+app.run(debug=True)
